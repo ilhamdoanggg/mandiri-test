50421b915dc5be6a7c65c23b5438b59a590c0b93
ilham doang <ilhamdoanggg@gmail.com>
2020-01-23 16:14:03 +0700

    [Feat] able documentation API

--- src/main/java/com/music/management/MusicApplication.java
+++ src/main/java/com/music/management/MusicApplication.java
@@ -2,8 +2,10 @@ package com.music.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MusicApplication {
 
     public static void main(String[] args) {
